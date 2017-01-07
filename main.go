afdb79a1d0ece6b42548f322c0bc546e2551821a
Doug <doug.safford@yahoo.com>
2017-01-07 14:37:14 -0600

    initial commit, initial test Handler in server.go works

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+"fmt"
+"os"
+
+"github.com/cloudfoundry-community/go-cfenv"
+"github.com/cloudnativego/gogo-service/service"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "3000"
+	}
+
+	appEnv, err := cfenv.Current()
+	if err != nil {
+		fmt.Println("CF Environment not detected.")
+	}
+
+	server := service.NewServer(appEnv)
+	server.Run(":" + port)
+}
+
